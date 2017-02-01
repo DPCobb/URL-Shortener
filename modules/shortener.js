7c75b8a8c5e42dec0ec712207ee8a009ee7ab02d
Dan Cobb <dancobb@gmail.com>
2017-02-01 17:21:50 -0500

    First pass as shortening using crypto module

--- modules/shortener.js
+++ modules/shortener.js
@@ -7,7 +7,14 @@
  */
 
 module.exports = (url) => {
+    var prefix = 'tyny.io/'
     var url = url.body.url
-    console.log(url)
-    return { url }
+    const crypto = require('crypto');
+    const hash = crypto.createHmac('sha256', url)
+                   .digest('hex');
+    url = hash.substr(0,6)
+    var short = prefix + url
+
+    console.log(short)
+    return { short }
 }
