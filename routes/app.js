8b79b91cdb7622a3c22c5473821cabf78b2be71f
Dan Cobb <dancobb@gmail.com>
2017-02-01 16:01:52 -0500

    Get server.js and app.js talking and seeing routes

--- routes/app.js
+++ routes/app.js
@@ -5,3 +5,10 @@
  * Assignment 1: Static API
  *
  */
+module.exports = function(express){
+    var router = express.Router()
+    router.get('/', function(req, res){
+        res.json({hello:'world'})
+    })
+    return router
+}
