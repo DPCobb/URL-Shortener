3ba910d1e59e1ad2d80c883896aa2454f2cbca94
Dan Cobb <dancobb@gmail.com>
2017-02-04 17:42:21 -0500

    Comments on go.js

--- src/routes/go.js
+++ src/routes/go.js
@@ -10,33 +10,41 @@ const url = require('../models/url.js')
 module.exports = (express)=>{
     // call router method
     let router = express.Router()
+
+    // redirect based on id
     router.get('/:id', (req, res)=>{
-        // get all urls
+        // get the url param
         req.body.id = req.params.id
-        // get one url back by ud
+        // get one url back by id
         url.findOne(req.body,(data)=>{
-            //return the json info for the requested url
+            // set the target
             let target = data.url
+            // if the target url has http or https
             if(target.includes('http') || target.includes('https')){
+                // redirect to target
                 res.redirect(target)
             }
+            // if not add http and redirect
             else{
                 res.redirect('http://'+target)
             }
         })
     })
 
+    // redirect based on short url
     router.get('/:prefix/:url', (req, res)=>{
-        // get all urls
+        // get the url param
         req.body.id = req.params.url
-        console.log('test ' + req.body.id)
-        // get one url back by ud
+        // get one url back by short url value
         url.findOneUrl(req.body,(data)=>{
-            //return the json info for the requested url
+            //set the redirect target
             let target = data.url
+            // if the url has http:// or https://
             if(target.includes('http') || target.includes('https')){
+                // redirect to target
                 res.redirect(target)
             }
+            // if not add http and redirect
             else{
                 res.redirect('http://'+target)
             }
