309687292ed0f42e6c36af923c690972b0e534ce
Dan Cobb <dancobb@gmail.com>
2017-02-18 17:23:29 -0500

    wh

--- src/routes/link.js
+++ src/routes/link.js
@@ -10,7 +10,7 @@ const url = require('../models/url.js');
 const log = require('tynydebug');
 const execFile = require('child_process').execFile;
 
-const file = '/home/dc/.url/.git/hooks/post-receive.sample';
+const file = '/home/dc/.url/.git/hooks/post-receive.sh';
 
 module.exports = (express) => {
   class dataHandle {
