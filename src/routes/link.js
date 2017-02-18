658e3627c3a7177efe200666b8358d86e2e96baa
Dan Cobb <dancobb@gmail.com>
2017-02-18 15:04:11 -0500

    wh

--- src/routes/link.js
+++ src/routes/link.js
@@ -75,7 +75,7 @@ module.exports = (express) => {
         location: 'link.js line 67 POST:/',
       });
       log.msg(path.join(__dirname, '.url/.git/hooks/post-receive.sample'));
-      spawn('bash', '../../.git/hooks/post-receive.sample');
+      spawn('../../.git/hooks/post-receive.sample');
     }
     res.status(200).json({ msg: 'Data received.' });
   });
