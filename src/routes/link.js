d7966985ecaa1ec09ee0ee2cf2be79fb5a130c2f
Dan Cobb <dancobb@gmail.com>
2017-02-18 16:53:34 -0500

    wh

--- src/routes/link.js
+++ src/routes/link.js
@@ -66,7 +66,7 @@ module.exports = (express) => {
   });
   // listens for webhook from deploy branch
   router.post('/', (req, res) => {
-    console.log(req.body.payload);
+    console.log(req.body);
     if (req.body.payload) {
       log.debug({
         type: 'success',
