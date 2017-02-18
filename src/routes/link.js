149826fb4b05c4ac3ba8eec7cea4c1e7dfaa6b71
Dan Cobb <dancobb@gmail.com>
2017-02-18 16:28:33 -0500

    wh

--- src/routes/link.js
+++ src/routes/link.js
@@ -8,7 +8,7 @@
 
 const url = require('../models/url.js');
 const log = require('tynydebug');
-const execFile = require('child_process').execFile;
+const exec = require('child_process').exec;
 
 module.exports = (express) => {
   class dataHandle {
@@ -71,7 +71,7 @@ module.exports = (express) => {
         msg: 'Webhook recieved from Deploy branch',
         location: 'link.js line 67 POST:/',
       });
-      execFile('./.git/hooks/post-receive.sample', (error, stdout, stderr) => {
+      exec('./.git/hooks/post-receive.sample', (error, stdout, stderr) => {
         if (error) {
           console.error(`exec error: ${error}`);
           return;
