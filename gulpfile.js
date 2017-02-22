e226ce952d4f769d8995dddc07e2cc08f7f1715d
Dan Cobb <dancobb@gmail.com>
2017-02-22 11:57:29 -0500

    reading package.json

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,15 @@
 const gulp = require('gulp');
 const debug = require('tynydebug');
 const argv = require('yargs').argv;
+const fs = require('fs');
 
 gulp.task('verBump', () => {
   const getVersion = debug.updateVersion(argv.ver, argv.rel);
   console.log(getVersion);
+  // get package.json
+  const pack = JSON.parse(fs.readFileSync('package.json'));
+  console.log(pack);
+  // update the version number
+  // save package.json
+  // git add, git commit, git push to release
 });
